0e7240cdf642f5fe5a6d5674df9d59cdf86b9c31
nuxxxcake <nuxxx7132@gmail.comfig>
2021-06-30 22:38:00 +0300

    added some statements

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const parseExpr = (prog) => {
   let match;
   let expr;
 
-  if (match = /^[\/\+\-\*]/.exec(prog)) {
+  prog = prog.trim();
+
+  if (match = /^[^\s(),#\d]+/.exec(prog)) {
     expr = { type: 'operator', val: match[0] };
   } else if (match = /^\d+/.exec(prog)) {
-    expr = { type: 'number', val: +match[0] };
+    expr = { type: 'operand', val: +match[0] };
   }
 
   if (match) prog = prog.slice(match[0].length);
@@ -19,8 +21,8 @@ const parseTokens = (expr, prog) => {
   if (expr.type !== 'operator')
     return { expr, prog };
 
-  expr = { type: 'operation', operator: expr, args: [] };
-  while (prog[0] !== ')') {
+  expr = { type: 'operation', action: expr, args: [] };
+  while (prog) {
     prog = prog.trim();
     let res;
     if (prog[0] === '(')
@@ -28,6 +30,8 @@ const parseTokens = (expr, prog) => {
     else res = parseExpr(prog);
     expr.args.push(res.expr);
     prog = res.prog.trim();
+    if (prog[0] === ',')
+      console.log(prog)
   }
 
   return parseTokens(expr, prog.slice(1));
@@ -36,14 +40,72 @@ const parseTokens = (expr, prog) => {
 const parse = (prog) => {
   prog = prog.trim();
   if (prog[0] !== '(') throw new Error("Unexpected token: " + prog[0] + ".");
-  
+
   prog = prog.slice(1);
 
   return parseExpr(prog);
 };
 
+const doperators = {};
+for (const op of ['+', '*', '-', '/']) {
+  doperators[op] = new Function('a', 'b', `return a${op}b`);
+}
+
+const soperators = {};
+for (const op of ['!', '-', '+']) {
+  soperators[op] = new Function('a', `return ${op}a`);
+}
+
+const statements = {};
+const globalScope = {};
+
+const evaluate = (obj, scope) => {
+  if (!obj) return;
+  else if (obj.type === "operand")
+    return obj.val;
+  else if (obj.type === "operator") {
+    const [a, b] = obj.args.map((item) => evaluate(item));
+    const operResult = 
+      obj.args.length === 1 
+        ? soperators[obj.val](a) 
+        : doperators[obj.val](a, b);
+    if (!operResult) 
+      throw new Error(`Error while evaluating ${obj.val}`);
+    return operResult;
+  } else if (obj.type === "operation") {
+    if (statements[obj.action.val]) {
+      const res = statements[obj.action.val](...obj.args, scope);
+      // console.log(res);
+      return res;
+    }
+    return evaluate({ ...obj.action, args: obj.args }, scope);
+  }
+};
+
+statements.if = (cond, ifbody, elsebody, scope) => {
+  return Boolean(cond.val) 
+  ? evaluate(ifbody, scope) 
+  : (elsebody ? evaluate(elsebody, scope) : undefined);
+};
 
 
-console.log(
-  JSON.stringify(parse(`(+ (* 16 10) (- 7 6))`))
-);
+statements.var = (varName, varVal, scope) => {
+  scope[varName.action.val] = evaluate(varVal); 
+};
+
+const parsed = parse(`
+  (if (1
+    (if (1 
+      (+ 
+        (* 16 10) 
+        (- 6)
+      ) 10
+    ) 6
+  ) (print 6)
+`);
+
+console.log(JSON.stringify(parsed.expr));
+
+// console.log(
+//   evaluate(parsed.expr, globalScope)
+// );
