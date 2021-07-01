faece05986af7056f0568a9e888c9c33d8cac7e0
nuxxxcake <nuxxx7132@gmail.comfig>
2021-07-01 14:36:33 +0300

    some rework and added statements

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+
+const statements = {};
+const globalScope = {};
+
+const doperators = {};
+for (const op of ['+', '*', '-', '/', '>', '!=', '<', '>=', '<=', '==']) {
+  doperators[op] = new Function('a', 'b', `return a${op}b`);
+}
+
+const soperators = {};
+for (const op of ['!', '-', '+']) {
+  soperators[op] = new Function('a', `return ${op}a`);
+}
+
+statements.if = (scope, cond, ifbody, elsebody) => {
+  return Boolean(cond.val) 
+  ? evaluate(ifbody, scope) 
+  : (elsebody ? evaluate(elsebody, scope) : undefined);
+};
+
+
+statements.def = (scope, varName, varVal) => {
+  scope[varName.val] = evaluate(varVal); 
+};
+
+statements.do = (scope, ...args) => {
+  let res = null;
+  for (let arg of args) {
+    res = evaluate(arg, scope);
+  }
+  return res;
+};
+
+statements.print = (scope, ...args) => {
+  for (let arg of args) {
+    // console.log(arg);
+    if (arg.type === "var" && scope[arg.val]) console.log(scope[arg.val]);
+    else console.log(evaluate(arg, scope));
+  }
+};
+
+statements.while = (scope, cond, body) => {};
+
 const parseExpr = (prog) => {
   let match;
   let expr;
@@ -5,12 +48,16 @@ const parseExpr = (prog) => {
   prog = prog.trim();
 
   if (match = /^[^\s(),#\d]+/.exec(prog)) {
-    expr = { type: 'operator', val: match[0] };
+    if (!statements[match[0]] && !doperators[match[0]] && !soperators[match[0]])
+      expr = { type: 'var', val: match[0] }; 
+    else expr = { type: 'operator', val: match[0] };
   } else if (match = /^\d+/.exec(prog)) {
     expr = { type: 'operand', val: +match[0] };
   }
 
   if (match) prog = prog.slice(match[0].length);
+
+  prog = prog.trim();
 
   return parseTokens(expr, prog);
 };
@@ -22,16 +69,15 @@ const parseTokens = (expr, prog) => {
     return { expr, prog };
 
   expr = { type: 'operation', action: expr, args: [] };
-  while (prog) {
+  while (prog[0] !== ')') {
     prog = prog.trim();
     let res;
     if (prog[0] === '(')
       res = parse(prog);
     else res = parseExpr(prog);
+
     expr.args.push(res.expr);
     prog = res.prog.trim();
-    if (prog[0] === ',')
-      console.log(prog)
   }
 
   return parseTokens(expr, prog.slice(1));
@@ -39,73 +85,43 @@ const parseTokens = (expr, prog) => {
 
 const parse = (prog) => {
   prog = prog.trim();
-  if (prog[0] !== '(') throw new Error("Unexpected token: " + prog[0] + ".");
+  if (prog[0] !== '(') 
+    throw new Error("Unexpected token: " + prog[0] + ".");
 
   prog = prog.slice(1);
 
   return parseExpr(prog);
 };
 
-const doperators = {};
-for (const op of ['+', '*', '-', '/']) {
-  doperators[op] = new Function('a', 'b', `return a${op}b`);
-}
-
-const soperators = {};
-for (const op of ['!', '-', '+']) {
-  soperators[op] = new Function('a', `return ${op}a`);
-}
-
-const statements = {};
-const globalScope = {};
-
 const evaluate = (obj, scope) => {
   if (!obj) return;
-  else if (obj.type === "operand")
+  else if (obj.type === "operand") {
     return obj.val;
+  }
+  else if (obj.type === "var") {
+    if (scope[obj.val]) return scope[obj.val];
+    throw new Error("No variable with name " + obj.val + " has been found.");
+  }
   else if (obj.type === "operator") {
-    const [a, b] = obj.args.map((item) => evaluate(item));
+    const [a, b] = obj.args.map((item) => evaluate(item, scope));
     const operResult = 
       obj.args.length === 1 
         ? soperators[obj.val](a) 
         : doperators[obj.val](a, b);
-    if (!operResult) 
+    if (operResult === undefined) 
       throw new Error(`Error while evaluating ${obj.val}`);
     return operResult;
   } else if (obj.type === "operation") {
     if (statements[obj.action.val]) {
-      const res = statements[obj.action.val](...obj.args, scope);
-      // console.log(res);
-      return res;
+      // console.log(obj.args);
+      return statements[obj.action.val](scope, ...obj.args);
     }
     return evaluate({ ...obj.action, args: obj.args }, scope);
   }
 };
 
-statements.if = (cond, ifbody, elsebody, scope) => {
-  return Boolean(cond.val) 
-  ? evaluate(ifbody, scope) 
-  : (elsebody ? evaluate(elsebody, scope) : undefined);
-};
-
-
-statements.var = (varName, varVal, scope) => {
-  scope[varName.action.val] = evaluate(varVal); 
-};
-
 const parsed = parse(`
-  (if (1
-    (if (1 
-      (+ 
-        (* 16 10) 
-        (- 6)
-      ) 10
-    ) 6
-  ) (print 6)
+  (do (def yes 5) (print (> yes 6)))
 `);
 
-console.log(JSON.stringify(parsed.expr));
-
-// console.log(
-//   evaluate(parsed.expr, globalScope)
-// );
+evaluate(parsed.expr, globalScope);
